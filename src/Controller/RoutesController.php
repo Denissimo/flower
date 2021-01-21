a47e990018aadc59ef7145a7342fb77cb79efd90
Den <droden@narod.ru>
2021-01-21 21:17:32 +0300

    entity

--- src/Controller/RoutesController.php
+++ src/Controller/RoutesController.php
@@ -7,11 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RoutesController extends AbstractController
 {
-    public function test(): Response
+    public function test(string $id): Response
     {
         $number = random_int(0, 100);
 
         return $this->render('test.html.twig', [
+            'id' => $id,
             'number' => $number,
         ]);
     }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,7 +4,80 @@
 namespace App\Entity;
 
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
 class Category
 {
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name_rus", type="string")
+     */
+    private $nameRus;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name_eng", type="string")
+     */
+    private $nameEng;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Bouquet", mappedBy="category")
+     */
+    private $bouquets;
+
+    public function __construct()
+    {
+        $this->bouquets = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameRus(): string
+    {
+        return $this->nameRus;
+    }
+
+    /**
+     * @param string $nameRus
+     */
+    public function setNameRus(string $nameRus): void
+    {
+        $this->nameRus = $nameRus;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameEng(): string
+    {
+        return $this->nameEng;
+    }
+
+    /**
+     * @param string $nameEng
+     */
+    public function setNameEng(string $nameEng): void
+    {
+        $this->nameEng = $nameEng;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getBouquets(): ArrayCollection
+    {
+        return $this->bouquets;
+    }
+
+    /**
+     * @param ArrayCollection $bouquets
+     */
+    public function setBouquets(ArrayCollection $bouquets): void
+    {
+        $this->bouquets = $bouquets;
+    }
 }
